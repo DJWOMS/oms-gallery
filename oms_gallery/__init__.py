b21b90527975f24882382094f1d4986214a97570
Omelchenko Michael <l2maximum@mail.ru>
2019-08-16 12:53:04 +0300

    add templates tags

--- oms_gallery/__init__.py
+++ oms_gallery/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.2.8'
+__version__ = '0.3'
 name = "oms_gallery"
 OMS_GALLERY_APP_DIR = os.path.dirname(os.path.abspath(__file__))
 default_app_config = 'oms_gallery.apps.GalleryConfig'
---
+++ oms_gallery/templatetags/gallery_tags.py
@@ -0,0 +1,39 @@
+from django import template
+
+from oms_gallery.models import Gallery, Photo
+
+register = template.Library()
+
+
+@register.simple_tag()
+def for_gallery(name=None, pk=None):
+    """Вывод галереи по имени или id"""
+    if name is not None:
+        gallery = Gallery.objects.filter(name=name)
+    elif pk is not None:
+        gallery = Gallery.objects.filter(id=pk)
+    else:
+        gallery = Gallery.objects.all()
+    return gallery
+
+
+@register.simple_tag()
+def get_photo(name=None, pk=None):
+    """Вывод фото по имени или id"""
+    if name is not None:
+        photo = Photo.objects.get(name=name)
+    elif pk is not None:
+        photo = Photo.objects.get(id=pk)
+    else:
+        photo = None
+    return photo
+
+
+@register.simple_tag()
+def for_photo(gallery=None):
+    """Вывод фото имени галереи"""
+    if gallery is not None:
+        photos = Photo.objects.filter(gallery__name=gallery)
+    else:
+        photos = Photo.objects.all()
+    return photos
